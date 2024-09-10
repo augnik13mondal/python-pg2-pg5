d3929c6256ca978756ed715a19e400b5a057275f
augnik13mondal <156066320+augnik13mondal@users.noreply.github.com>
2024-09-10 16:24:57 +0530

    Update program-6.py

--- program-6.py
+++ program-6.py
@@ -17,3 +17,26 @@ print(df.tail(5))
 
 
 ********(changes are here ::    'D:/Downloads/bca a.csv')******
+
+
+
+
+output:   First 5 Lines:
+   bca a  Unnamed: 1  Unnamed: 2  ...  Unnamed: 24  Unnamed: 25  Unnamed: 26
+0      1         1.0         2.0  ...         24.0         25.0         26.0
+1      2         NaN         NaN  ...          NaN          NaN          NaN
+2      3         NaN         NaN  ...          NaN          NaN          NaN
+3      4         NaN         NaN  ...          NaN          NaN          NaN
+4      5         NaN         NaN  ...          NaN          NaN          NaN
+
+[5 rows x 27 columns]
+
+Last 5 Lines:
+    bca a  Unnamed: 1  Unnamed: 2  ...  Unnamed: 24  Unnamed: 25  Unnamed: 26
+15     16         NaN         NaN  ...          NaN          NaN          NaN
+16     17         NaN         NaN  ...          NaN          NaN          NaN
+17     18         NaN         NaN  ...          NaN          NaN          NaN
+18     19         NaN         NaN  ...          NaN          NaN          NaN
+19     20         NaN         NaN  ...          NaN          NaN          NaN
+
+[5 rows x 27 columns]
