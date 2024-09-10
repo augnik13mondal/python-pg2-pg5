6a53b6ee1cc68584e15a6da008b753a271203e8b
augnik13mondal <156066320+augnik13mondal@users.noreply.github.com>
2024-09-10 16:24:13 +0530

    Create program-6.py

---
+++ program-6.py
@@ -0,0 +1,19 @@
+import pandas as pd
+# Replace 'your_file.csv' with the actual path to your CSV file
+file_path = 'D:/Downloads/bca a.csv'
+# Read the CSV file into a pandas DataFrame
+df = pd.read_csv(file_path)
+# Print the first 5 lines (+header) of the CSV file
+print("First 5 Lines:")
+print(df.head(5))
+# Print the last 5 lines (+header) of the CSV file
+print("\nLast 5 Lines:")
+print(df.tail(5))
+
+
+
+
+
+
+
+********(changes are here ::    'D:/Downloads/bca a.csv')******
